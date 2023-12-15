b6dca17ebe246f046a65418f6fad7bba29f669e9
ana.ujevic1 <ana.ujevic1@gmail.com>
2023-12-15 23:05:18 +0100

    drugi dio UZOP projekta

--- ana-priprema-podataka.py
+++ ana-priprema-podataka.py
@@ -135,7 +135,6 @@ X.describe()
 # %% [markdown]
 # - primjećujemo da je min(Final_margin - razlika u rezultatu) ima negativnu vrijednost -> možemo znati je li domaći tim dobio ili izgubio, ali upitna je smisao zbog te nagativne vrijednosti
 # - touch_time ima negativnu vrijednost
-# - primjećujemo da značajka period ima čudnu max vrijednost 7, a moguće je samo 4
 
 # %% [markdown]
 # # Vizualizacija podataka
@@ -163,13 +162,6 @@ sns.boxplot(X['TOUCH_TIME'])
 # %%
 sns.boxplot(X['PERIOD']) 
 plt.show()
-
-# %% [markdown]
-# - znamo da postoji ukupno 4 perioda te izbacujemo sve primjere gdje je period > 4
-
-# %%
-X = X.loc[(X.PERIOD <= 4)]
-sns.boxplot(X['PERIOD'])
 
 # %% [markdown]
 # ## Vizualizacija korelacija
@@ -250,9 +242,9 @@ plt.show()
 # ## Matrica dijagrama raspršenja
 
 # %%
-sns.set(rc={'figure.figsize':(25,15)})
-sns.pairplot(X.loc[:,['FINAL_MARGIN', 'SHOT_NUMBER', 'SHOT_CLOCK', 'DRIBBLES', 'TOUCH_TIME', 'SHOT_DIST', 'SHOT_RESULT']], hue="SHOT_RESULT", corner=True)
-plt.show()
+#sns.set(rc={'figure.figsize':(25,15)})
+#sns.pairplot(X.loc[:,['FINAL_MARGIN', 'SHOT_NUMBER', 'SHOT_CLOCK', 'DRIBBLES', 'TOUCH_TIME', 'SHOT_DIST', 'SHOT_RESULT']], hue="SHOT_RESULT", corner=True)
+#plt.show()
 
 # %% [markdown]
 # ## Zadnji pregled pogodataka
@@ -266,5 +258,533 @@ X.drop(('PTS_TYPE'), axis=1, inplace=True) #izbacujem jer direktno ovisi o SHOT_
 
 # %%
 X. describe()
+
+# %% [markdown]
+# # Replikacija rezultata
+
+# %% [markdown]
+# ### - dodatno čićenje
+
+# %%
+#Convert game clock to seconds
+#X['GAME_CLOCK'] = X['GAME_CLOCK'].apply(
+  #  lambda x: 60*int(x.split(':')[0]) + int(x.split(':')[1]))
+
+# %%
+X.describe()
+
+# %% [markdown]
+# ## 1. Logistička regresija
+
+# %% [markdown]
+# - Logistička regresija generira linearni set težina θ
+# - Mičem kategoričke varijable koje imaju više od dvije kategorije - to uključuje "GAME ID", "MATCHUP", "CLOSEST DEFENDER", "CLOSEST DEFENDER PLAYER ID", 
+# "player name" i "player id". Razlog je što kada sam zamijenila kategorije s cjelobrojnim vrijednostima te 
+# bi mogle zbuniti model koji pokušava naučiti linearne težine.
+
+# %%
+print(X.columns)
+
+# %%
+# Pretvaranje varijable X u DataFrame T
+T = pd.DataFrame(X)
+
+# %%
+print(T.columns)
+
+# %%
+# Uklanjanje određenih stupaca iz tablice
+columns_to_drop = ['CLOSEST_DEFENDER_PLAYER_ID']
+T = T.drop(columns=columns_to_drop)
+T.describe()
+
+# %%
+T.head()
+
+
+# %%
+# Pretvaranje vremena u sekunde
+def calc_secs(time_str):
+    hours, minutes = map(int, time_str.split(':'))
+    return (12 * 60 - 60 * hours + minutes)
+
+T['PERIOD'] = (T['PERIOD'] - 1) * (12 * 60)
+T['GAME_CLOCK'] = T['GAME_CLOCK'].apply(calc_secs)
+T['GAME_TIME'] = T['PERIOD'] + T['GAME_CLOCK']
+T = T.drop(columns=['PERIOD', 'GAME_CLOCK', 'CLOSEST_DEFENDER' ])
+
+# Pretvaranje kategoričkih varijabli u numeričke
+T['LOCATION'] = T['LOCATION'].apply(lambda x: 1 if x == 'A' else -1)
+T['W'] = T['W'].apply(lambda x: 1 if x == 'W' else -1)
+T['SHOT_RESULT'] = T['SHOT_RESULT'].apply(lambda x: 1 if x == 'made' else -1)
+
+# %%
+T.head()
+
+# %% [markdown]
+# - kombinirala sam značajku PERIOD i GAME_CLOCK  u jednu značajku: "UKUPNO VRIJEME IGRE". 
+# - "GAME CLOCK" značajka odbrojava i resetira se nakon svakog perioda, ali ona mora biti takva da razlikuje 10 sekundi preostalih u prvom periodu
+# od 10 sekundi preostalih u četvrtom periodu ( kada su igrači umorniji i možda skloniji promašiti šut).
+#
+
+# %%
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+# Provjera postojanja kolone 'SHOT_RESULT'
+if 'SHOT_RESULT' in T.columns:
+    # Izdvajanje stupca SHOT_RESULT u varijablu y
+    y = T['SHOT_RESULT']
+
+    # Makni stupac SHOT_RESULT iz dataseta T
+    T.drop(columns=['SHOT_RESULT'], inplace=True)
+
+    # Provjera nedostajućih vrednosti u podacima
+    if T.isnull().values.any() or y.isnull().values.any():
+        print("Postoje nedostajuće vrednosti u podacima")
+    else:
+        # Podijeli podatke T i y u omjeru 50:50 za trening i test skup
+        T_train, T_test, Y_train, Y_test = train_test_split(T, y, test_size=0.5, random_state=42)
+
+        # Inicijalizacija i treniranje modela s regularizacijom lambda=0.001
+        clf = LogisticRegression(C=1/0.001)
+        clf.fit(T_train, Y_train)
+
+        # Predviđanje na testnom skupu
+        predictions = clf.predict(T_test)
+
+        # Računanje confusion matrix
+        cm = confusion_matrix(Y_test, predictions)
+        print("Confusion Matrix:")
+        print(cm)
+
+        # Računanje accuracy score
+        accuracy = accuracy_score(Y_test, predictions)
+        print("Accuracy Score:", accuracy)
+else:
+    print("Kolona 'SHOT_RESULT' nije pronađena")
+
+# %% [markdown]
+# ## 2. Naivni Bayesov klasifikator
+
+# %% [markdown]
+# - Naivni Bayes je relativno jednostavan algoritam koji pretpostavlja da su osobine xi uvjetno nezavisne uz uvjet y
+# - Podatke kontinuiranih vrijedosti podijeljene su u 4 kategorije:
+#     - vise od 1 standardna devijacija ispod prosjeka
+#     - 1 standardna devijacija ispod prosjeka
+#     - 1 standardna devijacija iznad prosjeka,
+#     - vise od 1 standardna devijacija iznad prosjeka. 
+# - Nažalost, ta podjela nije pokazala primjetnu razliku u rezultatima koristeći Naivni Bayes.
+# - Binning -> Ova funkcija radi s kontinuiranim podacima poput brojeva, primjerice, duljina šuteva u
+#   košarci ili vrijeme provedeno s loptom. Ona uzima te brojeve i sortira ih u četiri kategorije kako bi Naivni Bayes model mogao bolje razumjeti i naučiti iz tih grupa umjesto iz točnih vrijednosti.
+#
+#
+#
+
+# %%
+import numpy as np
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+
+# Podaci su već definirani u varijablama T i y
+# T i y su vaši stvarni podaci koje želite podijeliti na trening i test skupove
+
+# Podijela podataka na trening i test skupove
+T_train, T_test, Y_train, Y_test = train_test_split(T, y, test_size=0.5)
+
+# Funkcija za biniranje kontinuiranih varijabli u 4 kategorije
+
+def bin_continuous_data(data):
+    data_abs = np.abs(data)
+    means = np.mean(data_abs, axis=0)
+    stds = np.std(data_abs, axis=0)
+
+    boundaries = np.array([means - stds, means - 0.5 * stds, means + 0.5 * stds, means + stds])
+
+    categories = np.zeros_like(data, dtype=int)
+    for i in range(data.shape[1]):
+        for j in range(4):
+            categories[:, i] += (data[:, i] > boundaries[j, i])
+
+    return categories
+
+# Binarizacija podataka
+T_train_binned = bin_continuous_data(T_train.values)
+T_test_binned = bin_continuous_data(T_test.values)
+
+# Treniranje multinomijalnog Naivnog Bayesa
+clf = MultinomialNB(alpha=1.0)
+clf.fit(T_train_binned, Y_train)
+
+# Cross-validation
+scores = cross_val_score(clf, T_train_binned, Y_train, cv=5)
+print("Cross-validation scores:", scores)
+
+# Predviđanje na testnom skupu
+predictions = clf.predict(T_test_binned)
+
+# Računanje točnosti
+accuracy = accuracy_score(Y_test, predictions)
+print('Test accuracy:', accuracy)
+
+
+conf_matrix = confusion_matrix(Y_test, predictions)
+print('Confusion Matrix:')
+print(conf_matrix)
+
+# %% [markdown]
+# ## 3. Random Forest klasifikator
+
+# %% [markdown]
+# - Random Forest klasiifikator dobro obrađuju kategoričke podatke zbog prirode stabala odlučivanja.
+# Također je otporan  na prenaučenost jer koristi više stabala odlučivanja za klasifikaciju podataka.
+# - algoritam ansambla koji nasumično bira podskup značajki, stvara optimalno stablo odlučivanja od tih značajki, zatim ponavlja proces s novim podskupovima značajki kako bi na kraju stvorio "šumu" stabala odlučivanja
+
+# %%
+Z = pd.read_csv("shot_logs.csv")
+Z.drop(('GAME_ID'), axis=1, inplace=True)
+Z.drop(('player_id'), axis=1, inplace=True)
+Z.drop(('MATCHUP'), axis=1, inplace=True)
+Z.drop(('player_name'), axis=1, inplace=True)
+
+X_tmp = Z.copy()
+X_tmp = X_tmp.loc[X_tmp.SHOT_CLOCK.notnull(), :] ##izbacili smo primjere koji imaju vrijednost vremena koje je preostalo
+Z = X_tmp
+
+Z.drop(('FGM'), axis=1, inplace=True)
+Z.drop(('PTS'), axis=1, inplace=True)
+Z.drop(('PTS_TYPE'), axis=1, inplace=True) #izbacujem jer direktno ovisi o SHOT_DIST
+
+T = pd.DataFrame(Z)
+T.columns
+# Uklanjanje određenih stupaca iz tablice
+columns_to_drop = ['CLOSEST_DEFENDER_PLAYER_ID']
+T = T.drop(columns=columns_to_drop)
+T.describe()
+# Pretvaranje vremena u sekunde
+def calc_secs(time_str):
+    hours, minutes = map(int, time_str.split(':'))
+    return (12 * 60 - 60 * hours + minutes)
+
+T['PERIOD'] = (T['PERIOD'] - 1) * (12 * 60)
+T['GAME_CLOCK'] = T['GAME_CLOCK'].apply(calc_secs)
+T['GAME_TIME'] = T['PERIOD'] + T['GAME_CLOCK']
+T = T.drop(columns=['PERIOD', 'GAME_CLOCK', 'CLOSEST_DEFENDER' ])
+
+# Pretvaranje kategoričkih varijabli u numeričke
+T['LOCATION'] = T['LOCATION'].apply(lambda x: 1 if x == 'A' else -1)
+T['W'] = T['W'].apply(lambda x: 1 if x == 'W' else -1)
+T['SHOT_RESULT'] = T['SHOT_RESULT'].map({'made': 1, 'missed': -1})
+T
+
+# %%
+T
+
+# %%
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from scipy.stats import randint
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+# Provjera postojanja kolone 'SHOT_RESULT'
+if 'SHOT_RESULT' in T.columns:
+    # Izdvajanje stupca SHOT_RESULT u varijablu y
+    y = T['SHOT_RESULT']
+
+    # Makni stupac SHOT_RESULT iz dataseta T
+    T.drop(columns=['SHOT_RESULT'], inplace=True)
+
+    # Provjera nedostajućih vrednosti u podacima
+    if T.isnull().values.any() or y.isnull().values.any():
+        print("Postoje nedostajuće vrednosti u podacima")
+    else:
+        # Podijeli podatke T i y u omjeru 50:50 za trening i test skup
+        T_train, T_test, Y_train, Y_test = train_test_split(T, y, test_size=0.5, random_state=42)
+
+        clf = RandomForestClassifier()
+        clf.fit(T_train, Y_train)
+
+        # Predviđanje na testnom skupu
+        predictions = clf.predict(T_test)
+
+        # Računanje confusion matrix
+        cm = confusion_matrix(Y_test, predictions)
+        print("Confusion Matrix:")
+        print(cm)
+
+        # Računanje accuracy score
+        accuracy = accuracy_score(Y_test, predictions)
+        print("Accuracy Score:", accuracy)
+else:
+    print("Kolona 'SHOT_RESULT' nije pronađena")
+
+# %% [markdown]
+# ## 4. SVM
+
+# %%
+Z = pd.read_csv("shot_logs.csv")
+Z.drop(('GAME_ID'), axis=1, inplace=True)
+Z.drop(('player_id'), axis=1, inplace=True)
+Z.drop(('MATCHUP'), axis=1, inplace=True)
+Z.drop(('player_name'), axis=1, inplace=True)
+
+X_tmp = Z.copy()
+X_tmp = X_tmp.loc[X_tmp.SHOT_CLOCK.notnull(), :] ##izbacili smo primjere koji imaju vrijednost vremena koje je preostalo
+Z = X_tmp
+
+Z.drop(('FGM'), axis=1, inplace=True)
+Z.drop(('PTS'), axis=1, inplace=True)
+Z.drop(('PTS_TYPE'), axis=1, inplace=True) #izbacujem jer direktno ovisi o SHOT_DIST
+
+T = pd.DataFrame(Z)
+T.columns
+# Uklanjanje određenih stupaca iz tablice
+columns_to_drop = ['CLOSEST_DEFENDER_PLAYER_ID']
+T = T.drop(columns=columns_to_drop)
+T.describe()
+# Pretvaranje vremena u sekunde
+def calc_secs(time_str):
+    hours, minutes = map(int, time_str.split(':'))
+    return (12 * 60 - 60 * hours + minutes)
+
+T['PERIOD'] = (T['PERIOD'] - 1) * (12 * 60)
+T['GAME_CLOCK'] = T['GAME_CLOCK'].apply(calc_secs)
+T['GAME_TIME'] = T['PERIOD'] + T['GAME_CLOCK']
+T = T.drop(columns=['PERIOD', 'GAME_CLOCK', 'CLOSEST_DEFENDER' ])
+
+# Pretvaranje kategoričkih varijabli u numeričke
+T['LOCATION'] = T['LOCATION'].apply(lambda x: 1 if x == 'A' else -1)
+T['W'] = T['W'].apply(lambda x: 1 if x == 'W' else -1)
+T['SHOT_RESULT'] = T['SHOT_RESULT'].map({'made': 1, 'missed': -1})
+T
+
+# %%
+from sklearn import svm
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import SGDClassifier
+from sklearn.svm import LinearSVC
+
+# Provjera postojanja kolone 'SHOT_RESULT'
+if 'SHOT_RESULT' in T.columns:
+    # Izdvajanje stupca SHOT_RESULT u varijablu y
+    y = T['SHOT_RESULT']
+
+    # Makni stupac SHOT_RESULT iz dataseta T
+    T.drop(columns=['SHOT_RESULT'], inplace=True)
+
+    # Provjera nedostajućih vrednosti u podacima
+    if T.isnull().values.any() or y.isnull().values.any():
+        print("Postoje nedostajuće vrednosti u podacima")
+    else:
+        # Podijeli podatke T i y u omjeru 50:50 za trening i test skup
+        T_train, T_test, Y_train, Y_test = train_test_split(T, y, test_size=0.5, random_state=42)
+
+        clf = make_pipeline(StandardScaler(),LinearSVC(dual=False, random_state=0, tol=1e-5))
+        clf.fit(T_train, Y_train)
+
+        # Predviđanje na testnom skupu
+        predictions = clf.predict(T_test)
+
+        # Računanje confusion matrix
+        cm = confusion_matrix(Y_test, predictions)
+        print("Confusion Matrix:")
+        print(cm)
+
+        # Računanje accuracy score
+        accuracy = accuracy_score(Y_test, predictions)
+        print("Accuracy Score:", accuracy)
+else:
+    print("Kolona 'SHOT_RESULT' nije pronađena")
+
+# %% [markdown]
+# ## 5. Neuralne mreže
+
+# %%
+Z = pd.read_csv("shot_logs.csv")
+Z.drop(('GAME_ID'), axis=1, inplace=True)
+Z.drop(('player_id'), axis=1, inplace=True)
+Z.drop(('MATCHUP'), axis=1, inplace=True)
+Z.drop(('player_name'), axis=1, inplace=True)
+
+X_tmp = Z.copy()
+X_tmp = X_tmp.loc[X_tmp.SHOT_CLOCK.notnull(), :] ##izbacili smo primjere koji imaju vrijednost vremena koje je preostalo
+Z = X_tmp
+
+Z.drop(('FGM'), axis=1, inplace=True)
+Z.drop(('PTS'), axis=1, inplace=True)
+Z.drop(('PTS_TYPE'), axis=1, inplace=True) #izbacujem jer direktno ovisi o SHOT_DIST
+
+T = pd.DataFrame(Z)
+T.columns
+# Uklanjanje određenih stupaca iz tablice
+columns_to_drop = ['CLOSEST_DEFENDER_PLAYER_ID']
+T = T.drop(columns=columns_to_drop)
+T.describe()
+# Pretvaranje vremena u sekunde
+def calc_secs(time_str):
+    hours, minutes = map(int, time_str.split(':'))
+    return (12 * 60 - 60 * hours + minutes)
+
+T['PERIOD'] = (T['PERIOD'] - 1) * (12 * 60)
+T['GAME_CLOCK'] = T['GAME_CLOCK'].apply(calc_secs)
+T['GAME_TIME'] = T['PERIOD'] + T['GAME_CLOCK']
+T = T.drop(columns=['PERIOD', 'GAME_CLOCK', 'CLOSEST_DEFENDER' ])
+
+# Pretvaranje kategoričkih varijabli u numeričke
+T['LOCATION'] = T['LOCATION'].apply(lambda x: 1 if x == 'A' else -1)
+T['W'] = T['W'].apply(lambda x: 1 if x == 'W' else -1)
+T['SHOT_RESULT'] = T['SHOT_RESULT'].map({'made': 1, 'missed': -1})
+T
+
+# %%
+y = T['SHOT_RESULT']
+
+# Makni stupac SHOT_RESULT iz dataseta T
+T.drop(columns=['SHOT_RESULT'], inplace=True)
+
+# %%
+#Treniranje modela
+
+# standardizacija
+scaler = StandardScaler()
+scaler.fit(T)
+T = scaler.transform(T)
+T
+
+# %%
+T
+
+# %%
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix
+
+
+relu_scores = np.array([])
+sigmoid_scores = np.array([])
+
+
+T_train, T_test, Y_train, Y_test = train_test_split(T, y, stratify=y)
+
+# default alpha je 0.0001
+clf_relu = MLPClassifier(random_state=1, max_iter=300, hidden_layer_sizes=[50], alpha=0.001).fit(T_train, Y_train) # RELU aktivacijska funkcija
+clf_sigmoid = MLPClassifier(random_state=1, max_iter=300, hidden_layer_sizes=[50], activation="logistic", alpha=0.001).fit(T_train, Y_train) # RELU aktivacijska funkcija!
+
+relu_scores = np.append(relu_scores, clf_relu.score(T_test, Y_test))
+sigmoid_scores = np.append(sigmoid_scores, clf_sigmoid.score(T_test, Y_test))
+
+print(f"relu_scores average: {np.average(relu_scores)}")
+print(f"sigmoid_scores average: {np.average(sigmoid_scores)}")
+
+T_train, T_test, Y_train, Y_test = train_test_split(T, y, stratify=y, test_size=0.1) 
+clf = MLPClassifier(random_state=1, max_iter=300, hidden_layer_sizes=[50], activation="logistic").fit(T_train, Y_train) 
+y_pred = clf.predict(T_test)
+print(confusion_matrix(Y_test,y_pred))
+print(clf.score(T_test, Y_test))
+
+# %% [markdown]
+# ## 6. XGBOOST
+
+# %%
+Z = pd.read_csv("shot_logs.csv")
+Z.drop(('GAME_ID'), axis=1, inplace=True)
+Z.drop(('player_id'), axis=1, inplace=True)
+Z.drop(('MATCHUP'), axis=1, inplace=True)
+Z.drop(('player_name'), axis=1, inplace=True)
+
+X_tmp = Z.copy()
+X_tmp = X_tmp.loc[X_tmp.SHOT_CLOCK.notnull(), :] ##izbacili smo primjere koji imaju vrijednost vremena koje je preostalo
+Z = X_tmp
+
+Z.drop(('FGM'), axis=1, inplace=True)
+Z.drop(('PTS'), axis=1, inplace=True)
+Z.drop(('PTS_TYPE'), axis=1, inplace=True) #izbacujem jer direktno ovisi o SHOT_DIST
+
+T = pd.DataFrame(Z)
+T.columns
+# Uklanjanje određenih stupaca iz tablice
+columns_to_drop = ['CLOSEST_DEFENDER_PLAYER_ID']
+T = T.drop(columns=columns_to_drop)
+T.describe()
+# Pretvaranje vremena u sekunde
+def calc_secs(time_str):
+    hours, minutes = map(int, time_str.split(':'))
+    return (12 * 60 - 60 * hours + minutes)
+
+T['PERIOD'] = (T['PERIOD'] - 1) * (12 * 60)
+T['GAME_CLOCK'] = T['GAME_CLOCK'].apply(calc_secs)
+T['GAME_TIME'] = T['PERIOD'] + T['GAME_CLOCK']
+T = T.drop(columns=['PERIOD', 'GAME_CLOCK', 'CLOSEST_DEFENDER' ])
+
+# Pretvaranje kategoričkih varijabli u numeričke
+T['LOCATION'] = T['LOCATION'].apply(lambda x: 1 if x == 'A' else -1)
+T['W'] = T['W'].apply(lambda x: 1 if x == 'W' else -1)
+T['SHOT_RESULT'] = T['SHOT_RESULT'].map({'made': 1, 'missed': 0})
+T = T.drop(columns=['LOCATION',	'W', 'FINAL_MARGIN', 'SHOT_NUMBER', 'DRIBBLES', 'GAME_TIME' ])
+T
+
+# %%
+
+# Pretvaranje negativnih vrijednosti u NaN
+T.loc[T['TOUCH_TIME'] < 0, 'TOUCH_TIME'] = np.nan
+
+# Dropanje redova s NaN vrijednostima u stupcu TOUCH_TIME
+T.dropna(subset=['TOUCH_TIME'], inplace=True)
+
+y = T['SHOT_RESULT']
+
+# Makni stupac SHOT_RESULT iz dataseta T
+T.drop(columns=['SHOT_RESULT'], inplace=True)
+
+# %%
+T.describe()
+
+# %%
+from xgboost import XGBClassifier
+from sklearn.metrics import confusion_matrix, mean_squared_error, precision_score
+from sklearn.model_selection import train_test_split
+
+#izvori koje sam koristila
+# https://www.kaggle.com/code/stuarthallows/using-xgboost-with-scikit-learn
+# https://www.kaggle.com/code/pablocastilla/predict-if-a-shot-is-made
+
+T_train, T_test, Y_train, Y_test = train_test_split(T, y, stratify=y, test_size=0.1)
+
+xgb_model = XGBClassifier(objective="binary:logistic").fit(T_train, Y_train)
+
+y_pred = xgb_model.predict(T_test)
+
+print(confusion_matrix(Y_test, y_pred))
+print(precision_score(Y_test, y_pred))
+
+# %% [markdown]
+# ## Optimizacija hiperparametara
+# - cilj nam je postici accuracy bliže 68%
+
+# %%
+from sklearn.model_selection import GridSearchCV
+
+#pretraživanje rešetke
+T_train, T_test, Y_train, Y_test = train_test_split(T, y, stratify=y, test_size=0.5)
+T_validation, T_test, Y_validation, Y_test = train_test_split(T, y, test_size=0.5, random_state=42)
+
+parameters_for_testing = {
+    'min_child_weight':[0.0001,0.001,0.01],
+    'eta':[0.00001,0.0001,0.001],
+    'n_estimators':[1,3,5,10],
+    'max_depth':[3,4]
+}
+
+xgb = XGBClassifier(objective="binary:logistic")
+
+grid_search = GridSearchCV(estimator = xgb, param_grid = parameters_for_testing, scoring='accuracy')
+grid_search.fit(T_train, Y_train)
+
+print(f'najbolji parametri {grid_search.best_params_}')
+print(f'najbolji score {grid_search.best_score_}')
 
 # %%
